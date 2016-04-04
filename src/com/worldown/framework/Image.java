8a558e9f5d3b5e63a4e68c5aceddb04070cf347e
worldown <d_dexter@mail.ru>
2016-04-04 12:47:48 +0300

    step 'Unity4-Day5' perfomed

---
+++ src/com/worldown/framework/Image.java
@@ -0,0 +1,10 @@
+package com.worldown.framework;
+
+import com.worldown.framework.Graphics.ImageFormat;;
+
+public interface Image {
+	public int getWidth();
+	public int getHeight();
+	public ImageFormat getFormat();
+	public void dispose();
+}
