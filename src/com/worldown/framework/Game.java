f5bd8085eefe1dcbe662ae3467353f1b8531c425
worldown <d_dexter@mail.ru>
2016-04-11 12:39:01 +0300

    fix class

--- src/com/worldown/framework/Game.java
+++ src/com/worldown/framework/Game.java
@@ -2,8 +2,8 @@ package com.worldown.framework;
 
 public interface Game {
 	public Audio getAudio();
-	public Input getInpute();
-	public FileIO getFIleIO();
+	public Input getInput();
+	public FileIO getFileIO();
 	public Graphics getGraphics();
 	public void setScreen(Screen screen);
 	public Screen getCurrentScreen();
